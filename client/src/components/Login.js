98abdcc7cc9a556204b7fd9c2b1baaedc106d571
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-07-10 21:12:47 -0500

    made login required before seeing anything else

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,15 @@
 const Login = () => {
-  return <h1 className="comp">Login</h1>
+  return (
+    <div className="d-flex justify-content-center align-items-center login-form">
+      <div>
+        <form className="form-control">
+          <input className="form-control form-item " placeholder="Username"></input>
+          <input className="form-control form-item" type="password" placeholder="Password"></input>
+          <button className="btn btn-primary form-item" type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  )
 };
 
 export default Login;
