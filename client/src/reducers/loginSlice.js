5aec95a93cd3d56d8346db85072f058bb9f87717
Emily Freeman <freeman.ecf@gmail.com>
2022-07-15 11:40:45 -0400

    initial redux setup

---
+++ client/src/reducers/loginSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+export const loginSlice = createSlice({
+  name: 'login',
+  initialState,
+  reducers: {
+    
+  }
+})
+
+export default loginSlice.reducer
