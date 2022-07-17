b5486e59a8e64eb3def8282615fbb8f2aca9adb3
Emily Freeman <freeman.ecf@gmail.com>
2022-07-17 11:26:16 -0400

    hardcoded user

--- client/src/reducers/loginSlice.js
+++ client/src/reducers/loginSlice.js
@@ -1,12 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:8000';
 
 const initialState = [];
+
+export const setMember = createAsyncThunk('login/setMember', async (hardCodedUserId) => {
+  try {
+    const response = await axios.get(baseUrl + '/members/' + hardCodedUserId);
+    return response.data
+  }
+  catch (err) {
+    return err
+  }
+})
 
 export const loginSlice = createSlice({
   name: 'login',
   initialState,
   reducers: {
     
+  },
+  extraReducers: (builder) => {
+    builder.addCase(setMember.fulfilled, (state, action) => {
+      return action.payload
+    })
   }
 })
 
