3dd4cf89ad08a0bafe05f567f3abf7e721ed49c0
Emily Freeman <freeman.ecf@gmail.com>
2022-07-18 18:03:44 -0400

    redux loads boards when workspace clicked

--- client/src/components/WorkspaceList.js
+++ client/src/components/WorkspaceList.js
@@ -1,19 +1,46 @@
 import Workspace from "./Workspace";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { loadBoardsInWorkspace, boardsLoaded } from "../reducers/workspaceSlice";
+import { useNavigate } from "react-router";
 
 const WorkspaceList = () => {
+  const dispatch = useDispatch();
 
   const workspacesLoaded = useSelector(state => state.home.workspacesLoaded);
   const workspaces = useSelector(state => state.home.workspaces);
+  const navigate = useNavigate();
 
   const renderWorkspaces = () => {
     if (workspacesLoaded) {
-      return workspaces.map((workspace) => {
+      return workspaces.map((workspace, i) => {
         return (
-          <div>{workspace.title}</div>
+          <div key={i} onClick={handleClick}>{workspace.title}</div>
         )
       })
     }
+  }
+
+  const handleClick = (e) => {
+    const titleClicked = e.target.innerHTML;
+
+    const workspaceClicked = workspaces.find((workspace) => {
+      return workspace.title === titleClicked
+    })
+
+    const workspaceId = workspaceClicked._id;
+    console.log(workspaceId)
+
+    const boards = workspaceClicked.boards
+    for (let i = 0; i < boards.length; i++) {
+      let board = boards[i];
+      dispatch(loadBoardsInWorkspace(board)).then((payload) => {
+        if (i === boards.length - 1) {
+          dispatch(boardsLoaded())
+        }
+      });
+    }
+
+    navigate("/boards")
   }
 
   return (
