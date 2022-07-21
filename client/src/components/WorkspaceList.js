4cc1a070946cb32875e6ef5ce68a7a4e0d743f0c
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-07-21 12:06:31 -0500

    members added to homepage

--- client/src/components/WorkspaceList.js
+++ client/src/components/WorkspaceList.js
@@ -1,7 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
-import { loadBoardsInWorkspace, getMembersOfWorkspace, boardsLoaded, clearBoards } from "../reducers/workspaceSlice";
+import { loadBoardsInWorkspace, getMembersOfWorkspace, boardsLoaded, clearBoards, clearMembers } from "../reducers/workspaceSlice";
 import { useNavigate } from "react-router";
 import { useEffect } from "react";
+
+export const randomColorGenerator = () => {
+  return Math.floor(Math.random()*16777215).toString(16);
+}
 
 const WorkspaceList = () => {
   const dispatch = useDispatch();
@@ -12,14 +16,15 @@ const WorkspaceList = () => {
 
   const workspacesLoaded = useSelector(state => state.home.workspacesLoaded);
   const workspaces = useSelector(state => state.home.workspaces);
+  const members = useSelector(state => state.workspace.members)
   const navigate = useNavigate();
 
   const renderWorkspaces = () => {
     if (workspacesLoaded) {
       return workspaces.map((workspace, i) => {
         const initial = workspace.title[0];
-        const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        const randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+        const randomColor = randomColorGenerator();
+        const randomColor2 = randomColorGenerator();
         const pfpStyle = {backgroundImage: "linear-gradient(#" + randomColor + ", #" + randomColor2 + ")"}
         return (
           <div className="workspace-div" key={i}>
@@ -30,12 +35,30 @@ const WorkspaceList = () => {
             <div className="row ws-buttons">
               <div className="ws-members col" onClick={handleClickOnMembers}>
                 {/*eslint-disable-next-line jsx-a11y/alt-text*/}
-                <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" />Members</div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" />Members
+                
+              </div>
               <div className="ws-boards col"  onClick={handleClickOnBoards}>
                 {/*eslint-disable-next-line jsx-a11y/alt-text*/}
                 <img src="https://icons.veryicon.com/png/o/miscellaneous/linear-icon-25/bulletin-board-4.png"/>Boards</div>
+              <div className="members-dropdown">
+                {renderMembers()}
+              </div>
             </div>
           </div>
+        )
+      })
+    }
+  }
+
+  const renderMembers = () => {
+    if(members[0]){
+      return members.map((member) => {
+        const randomColor = randomColorGenerator();
+        const colorStyle = { backgroundColor: "#" + randomColor };
+
+        return (
+          <div onClick={() => navigate(`../${member._id}`, {replace: true})} style={colorStyle} className="member-style" >{member.name.first[0]}{member.name.last[0]}</div>
         )
       })
     }
@@ -61,18 +84,30 @@ const WorkspaceList = () => {
     navigate("/" + workspaceClicked._id + "/boards")
   }
 
-  const handleClickOnMembers = (e) => {
-    const titleClicked = e.target.parentElement.parentElement.childNodes[0].childNodes[1].innerHTML;
+  const toggleShow = (element) => {
+    if(element.classList.contains("show")){
+      return element.classList.remove("show");
+    }
+    if(!element.classList.contains("show")){
+      return element.classList.add("show")
+    }
+  }
 
+  const handleClickOnMembers = (e) => {
+    dispatch(clearMembers());
+
+    const titleClicked = e.target.parentElement.parentElement.childNodes[0].childNodes[1].innerHTML;
     const workspaceClicked = workspaces.find((workspace) => {
       return workspace.title === titleClicked
     })
-
+    const dropdown = e.target.parentElement.childNodes[2];
     const members = workspaceClicked.members
+
     for (let i = 0; i < members.length; i++) {
       let member = members[i];
       dispatch(getMembersOfWorkspace(member))
     }
+    toggleShow(dropdown);
   }
 
   return (
