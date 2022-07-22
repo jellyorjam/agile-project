10a6cee29f251445fbb2a6d140c2cf89484c495f
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-07-22 14:43:19 -0500

    messing around with profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,23 +1,63 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom"
-import { findMember } from "../reducers/homeSlice";
+import { findMember, memberLoaded, memberWSLoaded, setViewedMemberWorkspace } from "../reducers/homeSlice";
+import { random_rgba } from "./BoardList";
 import { randomColorGenerator } from "./WorkspaceList";
 
 const Profile = () => {
   const dispatch = useDispatch();
   const { userId } = useParams();
-
-  useEffect(() => {
-    dispatch(findMember(userId)).catch((err) => {
-      return err;
-    })
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const viewedMember = useSelector(state => state.home.viewedMember)
+  const viewedMember = useSelector(state => state.home.viewedMember);
+  const memberIsLoaded = useSelector(state => state.home.memberLoaded);
+  const memberWSReady = useSelector(state => state.home.memberWSLoaded);
+  const workspaces = useSelector(state => state.home.viewedMember.workspaces)
   const color = randomColorGenerator();
   const colorStyle = { backgroundColor: "#" + color }
+
+  useEffect(() => {
+    dispatch(findMember(userId)).then(
+      dispatch(memberLoaded())
+    ).catch((err) => {
+      return err;
+    })
+
+    if(memberIsLoaded){
+      workspaces.forEach((workspace) => {
+        dispatch(setViewedMemberWorkspace(workspace)).then(() => {
+          if(workspace.index === workspaces.length - 1){
+            return dispatch(memberWSLoaded()).catch(err => err)
+          }
+        }).catch((err) => {
+          return err;
+        });
+      })
+    }
+      
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    
+  const renderWorkspaces = () => {
+    if(memberWSReady) {
+      return workspaces.map((workspace, i) => {
+        const initial = workspace.title[0];
+        const random = random_rgba();
+        const pfpStyle = {backgroundColor: "rgba(" + random + "0.9)"}
+
+        return (
+          <div className="workspace-div" key={i}>
+            <div className="row main">
+              <div className="col-2"></div>
+              <div className="ws-initial col-1" style={pfpStyle}>{initial}</div>
+              <div className="ws-title col">{workspace.title}</div>
+            </div>
+          </div>
+        )
+      })
+    }
+
+  }
+
 
   
   if(viewedMember.name){
@@ -25,8 +65,12 @@ const Profile = () => {
       <div className="container profile-page">
         <img className="full-cover-pic" src={viewedMember.picture} alt={viewedMember.name.first} />
         <div style={colorStyle} className="full-pfp">{viewedMember.name.first[0]}{viewedMember.name.last[0]}</div>
-        <h1>{viewedMember.name.first} {viewedMember.name.last}<h3 className="email-profile">{<viewedMember className="login username"></viewedMember>}</h3></h1>
+        <h1>{viewedMember.name.first} {viewedMember.name.last}</h1>
         <h3 className="email-profile col">{viewedMember.email}</h3>
+        <h2>Workspaces</h2>
+        <div className="row card">
+          {renderWorkspaces()}
+        </div>
       </div>
   
     )
