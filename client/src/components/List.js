68284b0f5f65e53491b56a121b463538e3fc0196
Emily Freeman <freeman.ecf@gmail.com>
2022-07-23 10:46:44 -0400

    sorted cards to list

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -2,23 +2,47 @@ import Card from "./Card"
 import AddCard from "./AddCard"
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getLists, getCards, listsLoaded } from "../reducers/listSlice";
+import { getLists, getCards, listsLoaded, setListsAndCards} from "../reducers/listSlice";
+import axios from "axios";
 
 const List = () => {
   const dispatch = useDispatch();
   const lists = useSelector(state => state.board.boardInfo.lists);
-  console.log(lists)
+
 
   useEffect(() => {
-    lists.map((list) => {
-      return dispatch(getLists(list)).then((response) => {
-        const cards = response.payload.cards;
-        cards.map((card) => {
-          return dispatch(getCards(card))
-        })
-      })
-    })
+    getLists().then(() => getCards()).then(() => dispatch(setListsAndCards(returnedLists)));
   }, [])
+
+ const returnedLists = [];
+ 
+
+  const getLists = async () => {
+    for (let i = 0; i < lists.length; i++) {
+      let list = lists[i];
+      const response = await axios.get("http://localhost:8000/lists/" + list)
+      returnedLists.push({
+        list: response.data,
+        cards: []
+      })
+    }
+  }
+
+  const getCards = async () => {
+    for (let i = 0; i < returnedLists.length; i++) {
+    let cards = returnedLists[i].list.cards
+    let returnedCards = []
+      for (let i = 0; i < cards.length; i++) {
+        let card = cards[i]
+        const response = await axios.get("http://localhost:8000/cards/" + card)
+        returnedCards.push(response.data)
+      }
+      returnedLists[i].cards.push(returnedCards)
+    }
+  }
+
+ 
+
 
   return (
     <div className="comp">
--- client/src/reducers/listSlice.js
+++ client/src/reducers/listSlice.js
@@ -3,54 +3,55 @@ import axios from 'axios';
 
 const baseUrl = 'http://localhost:8000';
 
-const initialState = {
-  lists: [],
-  cards: []
-};
+const initialState = {}
+;
 
-export const getLists = createAsyncThunk('list/getLists', async (list) => {
-  try {
-    const response = await axios.get(baseUrl + '/lists/' + list);
-    return response.data
-  }
-  catch (err) {
-    return err
-  }
-})
+// export const getLists = createAsyncThunk('list/getLists', async (list) => {
+//   try {
+//     const response = await axios.get(baseUrl + '/lists/' + list);
+//     return response.data
+//   }
+//   catch (err) {
+//     return err
+//   }
+// })
 
-export const getCards = createAsyncThunk('list/getCards', async (card) => {
-  try {
-    const response = await axios.get(baseUrl + '/cards/' + card);
-    return {
-      _id: response.data._id,
-      title: response.data.title,
-      members: response.data.members,
-      labels: response.data.labels
-    }
-  }
-  catch (err) {
-    return err
-  }
-})
+// export const getCards = createAsyncThunk('list/getCards', async (card) => {
+//   try {
+//     const response = await axios.get(baseUrl + '/cards/' + card);
+//     return {
+//       _id: response.data._id,
+//       title: response.data.title,
+//       members: response.data.members,
+//       labels: response.data.labels
+//     }
+//   }
+//   catch (err) {
+//     return err
+//   }
+// })
 
 export const listSlice = createSlice({
   name: 'list',
   initialState,
   reducers: {
+  setListsAndCards: (state, action) => {
+    return action.payload
+  },
    listsLoaded: (state) => {
      state.listsLoaded = true
    },
    clearListsAndCards: () => initialState
   },
-  extraReducers: (builder) => {
-    builder.addCase(getLists.fulfilled, (state, action) => {
-        state.lists.push(action.payload)
-    });
-    builder.addCase(getCards.fulfilled, (state, action) => {
-      state.cards.push(action.payload)
-    })
-  }
+  // extraReducers: (builder) => {
+  //   builder.addCase(getLists.fulfilled, (state, action) => {
+  //     state.lists.push(action.payload)
+  //   });
+  //   builder.addCase(getCards.fulfilled, (state, action) => {
+  //     state.cards.push(action.payload)
+  //   })
+  // }
 })
 
-export const {listsLoaded, clearListsAndCards} = listSlice.actions;
+export const {listsLoaded, clearListsAndCards, setListsAndCards} = listSlice.actions;
 export default listSlice.reducer
