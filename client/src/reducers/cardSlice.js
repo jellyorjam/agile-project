0d7d36d7fd3c4b533e4adb6b9b3d45f9411a4e83
Emily Freeman <freeman.ecf@gmail.com>
2022-07-24 15:34:25 -0400

    redux works for cardDetails

--- client/src/reducers/cardSlice.js
+++ client/src/reducers/cardSlice.js
@@ -6,31 +6,18 @@ const baseUrl = 'http://localhost:8000';
 const initialState = {
 };
 
-// export const setWorkspaces = createAsyncThunk('home/setWorkspaces', async (workspace) => {
-//   try {
-//     const response = await axios.get(baseUrl + '/workspaces/' + workspace);
-//     return response.data
-//   }
-//   catch (err) {
-//     return err
-//   }
-// })
 
 export const cardSlice = createSlice({
   name: 'card',
   initialState,
   reducers: {
-    // workspacesLoaded: (state) => {
-    //   state.workspacesLoaded = true;
-    // },
-    // resetHome: () => initialState
+    setCardDetail: (state, action) => {
+      state.members = action.payload.members;
+      state.labels = action.payload.labels;
+      state.activity = action.payload.activity;
+    }
   },
-  // extraReducers: (builder) => {
-  //   builder.addCase(setWorkspaces.fulfilled, (state, action) => {
-  //     state.workspaces.push(action.payload);
-  //   });
-  // }
 })
 
-// export const {workspacesLoaded, resetHome} = homeSlice.actions;
+export const {setCardDetail} = cardSlice.actions
 export default cardSlice.reducer
