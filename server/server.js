1c28ca4f5102d9d35a418c6dbece4639d016d166
Emily Freeman <freeman.ecf@gmail.com>
2022-07-25 16:51:19 -0400

    fix static build

--- server/server.js
+++ server/server.js
@@ -38,14 +38,19 @@ app.use(
 if (process.env.NODE_ENV === "production") {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
-    app.use(express.static("../client/build"));
+    // app.use(express.static("../client/build"));
   
-    // Express will serve up the index.html file
-    // if it doesn't recognize the route
-    const path = require("path");
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
+    // // Express will serve up the index.html file
+    // // if it doesn't recognize the route
+    // const path = require("path");
+    // app.get("*", (req, res) => {
+    //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    // });
+
+    app.use(express.static(path.join(__dirname, '/../client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+      });
   }
 
 // router.get("/generate-fake-data", (req, res, next) => {
