2baa5c7b02eb004082a15ecaf0b369429507de12
Emily Freeman <freeman.ecf@gmail.com>
2022-07-25 16:55:18 -0400

    fix static build

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === "production") {
   
     // // Express will serve up the index.html file
     // // if it doesn't recognize the route
-    // const path = require("path");
+    const path = require("path");
     // app.get("*", (req, res) => {
     //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     // });
