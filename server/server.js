82997acee493c634d05393789d0dbdd5ffe89f33
Emily Freeman <freeman.ecf@gmail.com>
2022-07-26 15:55:16 -0400

    edit app.get(*)

--- server/server.js
+++ server/server.js
@@ -48,9 +48,9 @@ if (process.env.NODE_ENV === "production") {
     // });
 
     app.use(express.static(path.join(__dirname, '/../client/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-      });
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+    //   });
   }
 
 // router.get("/generate-fake-data", (req, res, next) => {
