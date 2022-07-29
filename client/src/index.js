cb3cf3cb5be7d0a68a5565be3af13932087488eb
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-07-28 22:42:04 -0500

    reseeded my own database and made a couple changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root')
+const root = createRoot(container)
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('root')
+  </Provider> 
 );
 
 reportWebVitals();
