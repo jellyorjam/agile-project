375f819190ce1e42a5c9583e306b3d789b146c71
Emily Freeman <freeman.ecf@gmail.com>
2022-07-28 21:47:14 -0400

    start work on editing card title

--- client/src/reducers/cardSlice.js
+++ client/src/reducers/cardSlice.js
@@ -8,6 +8,16 @@ const baseUrl = url;
 const initialState = {
 };
 
+// export const editCardTitle = createAsyncThunk('card/editTitle', async (list) => {
+//   try {
+//     const response = await axios.put(baseUrl + "/lists/" + list.list._id, list.list)
+//     return response.data;
+//   }
+//   catch (err) {
+//     return err;
+//   }
+// })
+
 
 export const cardSlice = createSlice({
   name: 'card',
